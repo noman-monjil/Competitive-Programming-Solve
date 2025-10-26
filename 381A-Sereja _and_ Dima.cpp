36daa26a2f31cd0059683b82dddc2f1c7e264f57
noman-monjil <abdulalnoman027@gmail.com>
2025-10-26 14:52:49 +0600

    Update 381A-Sereja _and_ Dima.cpp

--- 381A-Sereja _and_ Dima.cpp
+++ 381A-Sereja _and_ Dima.cpp
@@ -1,1 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;cin>>n;
+     vector<int>v(n);
+     for(int i=0;i<n;i++)
+     {
+         cin>>v[i];
+     }
+     int sereja=0;
+     int dim=0;
+
+        while(!v.empty())
+     {
+         if(v.front()<v.back())
+         {
+             sereja+=v.back();
+             v.pop_back();
+         }else{
+            sereja+=v.front();
+            v.erase(v.begin());
+
+         }
+
+         if(v.empty())break;
+
+
+          if(v.front()<v.back())
+         {
+             dim+=v.back();
+             v.pop_back();
+         }else{
+            dim+=v.front();
+            v.erase(v.begin());
+
+         }
+
+
+     }
+
+    cout<<sereja<<" "<<dim<<endl;
+}
+
+
 
